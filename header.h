4238f590c7be78dbe7d87aed2d5b18dac48bf31d
kmi0817 <62174395+kmi0817@users.noreply.github.com>
2021-06-16 18:26:24 +0900

    Add files via upload

---
+++ header.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#ifndef header_h
+#define header_h
+
+class BST; // 전방선언
+class Node {
+	friend BST; // friend 지정
+
+private:
+	int data;
+	Node* LeftChild, * RightChild;
+
+public:
+	Node(Node* left = 0, int e = 0, Node* right = 0);
+};
+
+class BST {
+private:
+	Node* root;
+	void inorder(Node* CurrentNode);
+
+public:
+	BST();
+	Node* IterSearch(const int& x);
+	bool Insert(const int& x);
+	Node* Delete(const int& x);
+
+	// 중위순회
+	void inorder();
+};
+#endif
+
